6a93d60e33e673eccc8c15914376786b53ce1c45
Anshuman Patnaik <anshuman.patnaik2008@gmail.com>
2017-09-16 01:07:42 -0400

    adding options for negation

--- demo.py
+++ demo.py
@@ -3,19 +3,25 @@ import cv2
 from pytesseract import *
 
 FILENAME = 'picture.jpg'
+negate = False
 
 def main():
     image = cv2.imread(FILENAME)
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
     # write gray file to a file
-    #cv2.imwrite('gray.jpg', gray)
+    if not negate:
+        gray_file = 'gray.jpg'
+        cv2.imwrite('gray.jpg', gray)
 
     # try inverted
-    negate_g = cv2.bitwise_not(gray)
-    cv2.imwrite('inverted_gray.jpg', negate_g)
+    if negate:
+        negate_g = cv2.bitwise_not(gray)
+        inverted_file = 'inverted_gray.jpg'
+        cv2.imwrite('inverted_file', negate_g)
+        gray_file = inverted_file
 
-    text = pytesseract.image_to_string(Image.open('inverted_gray.jpg'))
+    text = pytesseract.image_to_string(Image.open('gray_file.jpg'))
     import pdb ; pdb.set_trace()
 
 if __name__ == '__main__':
