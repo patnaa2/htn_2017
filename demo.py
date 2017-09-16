b24c100ef13126d2bbb89a350aa2a758f46b3698
Anshuman Patnaik <anshuman.patnaik2008@gmail.com>
2017-09-16 01:00:15 -0400

    removing linda's stuff

--- demo.py
+++ demo.py
@@ -5,10 +5,8 @@ from pytesseract import *
 FILENAME = 'picture.jpg'
 
 def main():
-    #image = cv2.imread(FILENAME)
-    #gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    img = Image.open(FILENAME).convert('LA')
-    img.save('temp.jpg')
+    image = cv2.imread(FILENAME)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
     # write gray file to a file
     #cv2.imwrite('gray.jpg', gray)
