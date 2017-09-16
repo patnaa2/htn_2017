9f6d89c09834d101bce0867d09573508eac9a240
Anshuman Patnaik <anshuman.patnaik2008@gmail.com>
2017-09-16 10:40:46 -0400

    attempt to get ruby to work on remot

--- server.py
+++ server.py
@@ -1,35 +1,53 @@
-import json
-import os
 from flask import Flask, request, redirect, url_for, send_from_directory, render_template
 from werkzeug import secure_filename
 import hashlib
+import json
+import os
+import subprocess
+import sys
+
+# set python path to current folder to local folder to add ruby lib
+sys.path.insert(0, os.getcwd())
+from exec_ruby import *
 
 UPLOAD_FOLDER = 'static/images/uploads'
 TMP_FOLDER = 'tmp'
 ALLOWED_EXTENSIONS = set(['jpg','jpeg', 'png'])
 
-app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.config['TMP_FOLDER'] = TMP_FOLDER
+def main():
+    app = Flask(__name__)
+    app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+    app.config['TMP_FOLDER'] = TMP_FOLDER
 
-if not os.path.exists(UPLOAD_FOLDER):
-    os.makedirs(UPLOAD_FOLDER)
-if not os.path.exists(TMP_FOLDER):
-    os.makedirs(TMP_FOLDER)
+    if not os.path.exists(UPLOAD_FOLDER):
+        os.makedirs(UPLOAD_FOLDER)
+    if not os.path.exists(TMP_FOLDER):
+        os.makedirs(TMP_FOLDER)
 
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+    def allowed_file(filename):
+        return '.' in filename and \
+               filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
-@app.route("/", methods=['GET', 'POST'])
-def index():
-    if request.method == 'POST':
-        f = request.files['file']
-        if f and allowed_file(f.filename):
-            filename = secure_filename(f.filename)
-            f.save(os.path.join(app.config['TMP_FOLDER'], filename))
-            return json.dumps({"val": "it works"}), 200
+    @app.route("/", methods=['GET', 'POST'])
+    def index():
+        if request.method == 'POST':
+            f = request.files['file']
+            if f and allowed_file(f.filename):
+                filename = secure_filename(f.filename)
+                f.save(os.path.join(app.config['TMP_FOLDER'], filename))
+                return redirect(url_for('results', filename=filename))
 
-    return render_template('index.html', title='Welcome')
+        # if request is a get
+        return render_template('index.html', title='Welcome')
 
-app.run()
+    @app.route("/results/<filename>")
+    def results(filename):
+        out, err = ruby(filename)
+        print out
+        print err
+        return json.dumps({"out": out, "err": err}), 200
+
+    app.run()
+
+if __name__ == '__main__':
+    main()
