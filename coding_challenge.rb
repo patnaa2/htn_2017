841aa24fc498016a85841b027c2a6b27b2e2a3e9
Trent Lavoie <trentlavoie@v1020-wn-127-12.campus-dynamic.uwaterloo.ca>
2017-09-16 23:23:40 -0400

    ruby challenge

---
+++ coding_challenge.rb
@@ -0,0 +1,14 @@
+# Coding Challenge:
+# Print Fizz if number is divisible by 3
+# Print Buzz if number is divisible by 5
+
+
+def fizzbuzz(num)
+puts "fizz" if num % 3 == 0
+puts "buzz" if num % 5 == 0
+end
+
+
+16.times do |n|
+  fizzbuzz(n)
+end
